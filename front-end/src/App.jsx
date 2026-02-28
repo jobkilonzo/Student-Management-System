572723c3ab8fac5d9bbe96771cecad3209f1e0b2
jobkilonzo <jobkilonzo95@gmail.com>
2026-02-28 11:53:57 +0300

    First commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+       <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
+
+    </>
+  )
+}
+
+export default App
